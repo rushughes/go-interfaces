a9a324e9630340e4ed4ea6c536abe74459a3ed89
Rus Hughes <russell.hughes@gmail.com>
2018-10-21 14:58:44 +0900

    s6 l54

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type bot interface {
+	getGreeting() string
+}
 
 type englishBot struct {
 }
@@ -16,12 +22,8 @@ func main() {
 	printGreeting(sb)
 }
 
-func printGreeting(eb englishBot) {
-	fmt.Println(eb.getGreeting())
-}
-
-func printGreeting(sb spanishBot) {
-	fmt.Println(sb.getGreeting())
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
 }
 
 func (englishBot) getGreeting() string {
